7efecd07c286f0100ea1ec33c4120d82b5e14c1b
Crystal Arnspiger <carnspiger@austin.utexas.edu>
2019-07-31 13:39:01 -0500

    requirements met

---
+++ python/app.py
@@ -0,0 +1,91 @@
+import ast
+from copy import deepcopy
+from datetime import date, datetime
+from dateutil.parser import parse
+from flask import Flask, jsonify, request, session
+from flask_cors import CORS
+from operator import itemgetter
+import requests
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+def main(argv):
+    global chosen_times
+    global unavailable
+
+
+@app.route("/today", methods=["GET"])
+def today():
+    return jsonify({"today": date.today().isoformat()})
+
+
+@app.route("/availability", methods=["GET"])
+def availability():
+    availability_by_id_dict = {}
+    response = requests.get('https://www.thinkful.com/api/advisors/availability')
+    availability_by_date = response.json()
+    global unavailable
+    try:
+        unavailable = unavailable
+    except NameError:
+        unavailable = []
+    for day in availability_by_date:
+        for timestamp in availability_by_date[day]:
+            opentime = parse(timestamp)
+            opentime = datetime.strftime(opentime, '%m/%d/%Y %I:%M %p')
+            advisor_id = availability_by_date[day][timestamp]
+            if (opentime, advisor_id) not in unavailable:
+                try:
+                    availability_by_id_dict[advisor_id]['opentimes'].append(opentime)
+                    availability_by_id_dict[advisor_id]['opentimes'].sort()
+                except KeyError:
+                    availability_by_id_dict[availability_by_date[day][timestamp]] = {
+                        'id': advisor_id,
+                        'opentimes': [opentime]
+                    }
+    availability_by_id = sorted(list(availability_by_id_dict.values()), key=itemgetter('id'))
+
+    return jsonify({'availability': availability_by_id})
+
+
+@app.route("/selected", methods=["POST"])
+def selected():
+    data = ast.literal_eval(request.data.decode())
+    student_name = data.get('student_name')
+    chosen_time = data.get('chosen_time')
+    advisor_id = data.get('advisor_id')
+
+    chosen_time_dict = {
+        'student_name': student_name,
+        'chosen_time': chosen_time,
+        'advisor_id': advisor_id
+    }
+    unavailable_time = (chosen_time, advisor_id)
+
+    global unavailable
+    try:
+        unavailable.append(unavailable_time)
+    except NameError:
+        unavailable = [unavailable_time]
+
+    global chosen_times
+    try:
+        chosen_times.append(chosen_time_dict)
+    except NameError:
+        chosen_times = [chosen_time_dict]
+
+    return 'Success'
+
+
+@app.route("/booked", methods=["GET"])
+def booked():
+    try:
+        global chosen_times
+        booked = chosen_times
+    except NameError:
+        booked = []
+
+    return jsonify({'booked': booked})
---
+++ web/src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.fetchToday();
+    this.fetchAvailability();
+    this.fetchBookedTimes();
+  }
+
+  async fetchToday() {
+    try {
+      const res = await fetch("http://localhost:4433/today");
+      const json = await res.json();
+      this.setState({today: json.today});
+    } catch (e) {
+      console.error("Failed to fetch 'today' data", e);
+    }
+  }
+
+  async fetchAvailability() {
+    try {
+      const res = await fetch("http://localhost:4433/availability");
+      const json = await res.json();
+      this.setState({availability: json.availability});
+    } catch (e) {
+      console.error("Failed to fetch availability data", e);
+    }
+  }
+
+  async fetchBookedTimes() {
+    try {
+      const res = await fetch("http://localhost:4433/booked");
+      const json = await res.json();
+      this.setState({booked: json.booked});
+    } catch (e) {
+      console.error("Failed to fetch reserved data", e);
+    }
+  }
+
+  handleClick = (advisor_id, chosen_time) => {
+    console.log("button clicked");
+    if (this.state.name) {
+      this.setState({formerror: null})
+      const data = {
+        "student_name": this.state.name,
+        "advisor_id": advisor_id,
+        "chosen_time": chosen_time
+      }
+      fetch("http://localhost:4433/selected",
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(data)
+        }
+      )
+      .then(() => {
+        this.fetchBookedTimes()
+        this.fetchAvailability();
+      });
+    } else {
+      this.setState({formerror: 'Please enter a name to book a time.'})
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App container">
+        <h1>Book Time with an Advisor</h1>
+        <div>
+          {this.state.today && <span id="today">Today is {this.state.today}.</span>}
+        </div>
+        <div>
+          {this.state.formerror &&
+          <p className="alert alert-danger">{this.state.formerror}</p>}
+        </div>
+        <form id="name-form" className="col-md-6">
+          <div className="form-group">
+            <label htmlFor="name-field">Your Name</label>
+            <input type="text" id="name-field" className="form-control"
+              onChange={ev => this.setState({name: ev.target.value})}/>
+          </div>
+        </form>
+
+
+        <h2>Available Times</h2>
+        <table className="advisors table">
+          <thead>
+            <tr>
+              <th>Advisor ID</th>
+              <th>Available Times</th>
+            </tr>
+          </thead>
+          {this.state.availability &&
+            <tbody>
+            {this.state.availability.map((advisor, key) => {
+              return (
+                <tr key={advisor.id}>
+                  <td>{advisor.id}</td>
+                  <td>
+                    <ul className="list-unstyled">
+                      {advisor.opentimes.map((opentime, index) => {
+                        return <li>
+                                <time dateTime={opentime} className="book-time">{opentime}</time>
+                                <button className="book btn-small btn-primary" onClick={this.handleClick.bind(this, advisor.id, opentime)}>Book</button>
+                              </li>
+                      })}
+                    </ul>
+                  </td>
+                </tr>
+              )
+              })
+            }
+            </tbody>
+          }
+        </table>
+
+
+        <h2>Booked Times</h2>
+        <table className="bookings table">
+          <thead>
+            <tr>
+              <th>Advisor ID</th>
+              <th>Student Name</th>
+              <th>Date/Time</th>
+            </tr>
+          </thead>
+          {this.state.booked &&
+            <tbody>
+            {this.state.booked.map((booked, key) => {
+              return (
+                <tr key={booked.chosen_time}>
+                  <td>{booked.advisor_id}</td>
+                  <td>{booked.student_name}</td>
+                  <td>{booked.chosen_time}</td>
+                </tr>
+              )
+              })
+            }
+            </tbody>
+          }
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
